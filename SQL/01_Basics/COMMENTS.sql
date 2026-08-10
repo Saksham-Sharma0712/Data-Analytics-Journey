b583f695bd53d1e17e78535291556d5d7ab5629b
Saksham-Sharma0712 <saksham.sh0712@gmail.com>
2026-08-10 19:22:23 +0530

    Add SQL comments practice
    Added examples of single-line and multi-line comments in SQL.

---
+++ SQL/01_Basics/COMMENTS.sql
@@ -0,0 +1,25 @@
+-- ============================================
+-- SQL COMMENTS
+-- ============================================
+
+-- Single-line comment
+-- Comments are ignored by SQL.
+-- They are used to explain the code.
+
+SELECT *
+FROM Employees;
+
+
+-- Multi-line comment
+/*
+   This is a multi-line comment.
+   It can contain multiple lines.
+*/
+
+SELECT *
+FROM Employees;
+
+
+-- Comment after a query
+SELECT *
+FROM Employees; -- Display all employees
